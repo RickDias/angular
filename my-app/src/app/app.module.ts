8a5acebd7637e620b6fe724c6b898167b6153623
GERSON HENRIQUE PEREIRA DIAS <gerson_dias@unimedmissoes.com.br>
2021-08-03 19:37:22 -0300

    nav component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule }         from './app-routing.module';
+import { AppComponent }             from './app.component';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+import { HomeComponent }            from './components/home/home.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent }  from './components/header/header.component';
+import { FooterComponent }  from './components/footer/footer.component';
 import { ContentComponent } from './components/content/content.component';
-import { NavComponent } from './components/nav/nav.component';
+import { NavComponent }     from './components/nav/nav.component';
+
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -19,13 +22,15 @@ import { NavComponent } from './components/nav/nav.component';
     HeaderComponent,
     FooterComponent,
     ContentComponent,
-    NavComponent
+    NavComponent,
+    MatListModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenav
   ],
   providers: [],
   bootstrap: [AppComponent]
